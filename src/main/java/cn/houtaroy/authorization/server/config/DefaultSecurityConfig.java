dd806a55ab57a1d66eaa3f3d341234095348aef0
houtaroy <houtaroy@gmail.com>
2024-11-26 21:33:59 +0800

    feat(仓库): 初始化项目

---
+++ src/main/java/cn/houtaroy/authorization/server/config/DefaultSecurityConfig.java
@@ -0,0 +1,30 @@
+package cn.houtaroy.authorization.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * 默认 Spring Security 配置类
+ */
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+public class DefaultSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
